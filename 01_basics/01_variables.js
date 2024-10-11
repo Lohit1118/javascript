c41a38aca77aa17b1710afe116a0384701ab1686
Lohit1118 <126514368+Lohit1118@users.noreply.github.com>
2024-10-11 10:35:06 +0000

    data types and variables repo

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "lohit@google.com"
+var accountPassword = "12345"
+accountCity = "berhmapur"
+let accountState;
+
+accountEmail = "loh@js.com"
+accountPassword = "234"
+accountCity = "bam"
+//accountId = 2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict "; // treat all code as newer version
+
+//alert(3+3)// mostly use in browser and we are using node js
+
+let name= "lohit"
+let age = 20
+let isLoggedIn = false
+
+console.log(typeof age);
