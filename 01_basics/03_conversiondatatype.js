0b686d503da5f1a279248030504b438bd47180c1
Lohit1118 <126514368+Lohit1118@users.noreply.github.com>
2024-10-13 07:20:56 +0000

    conversion of data types

---
+++ 01_basics/03_conversiondatatype.js
@@ -0,0 +1,13 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
