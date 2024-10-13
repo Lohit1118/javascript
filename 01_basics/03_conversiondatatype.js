0f92cba29ba75e1afa6b6c2ab87bb3aef480c5e4
Lohit1118 <126514368+Lohit1118@users.noreply.github.com>
2024-10-13 07:29:53 +0000

    modified

--- 01_basics/03_conversiondatatype.js
+++ 01_basics/03_conversiondatatype.js
@@ -11,3 +11,8 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let str1= "hello"
+let str2= "lohit"
+str3 = str1+str2;
+console.log(str3);
